69cdbd6b8a7601ee947453f44e8d47ba108ed5f3
Najumudeen Mohaideen Abdul Kader <nmohaideenabdulkader@manh.com>
2026-08-12 23:27:09 +0200

    Add API error handling

--- api_test.py
+++ api_test.py
@@ -1,22 +1,36 @@
 import requests
 
-response = requests.get("https://jsonplaceholder.typicode.com/posts")
+url = "https://this-domain-does-not-exist-12345.com"
 
-data = response.json()
 
-user_counts = {}
-count=0
-for post in data:
-    user_id = post['userId']
-    
-    if user_id in user_counts:
-        count += 1
-        user_counts[user_id]= count
-      
+try:
+    response = requests.get(url)
+    #print("Status code:",response.status_code)
+    if response.status_code == 200:
+      data = response.json()
+      print(data)
+    elif response.status_code == 404:
+      print("Post Not Found")
     else:
-        count = 1
-        user_counts[user_id] = count
-      
+      print("Request failed.")
+      print("Status Code",response.status_code)
 
-print(user_counts)
+except Exception as e:
+    print("Something went wrong", e)
+
+
+# # user_counts = {}
+# # count=0
+# # for post in data:
+# #     user_id = post['userId']
+    
+# #     if user_id in user_counts:
+# #         count += 1
+# #         user_counts[user_id]= count
+      
+# #     else:
+# #         count = 1
+# #         user_counts[user_id] = count
+      
+# # print(user_counts)
 
